504cceb7f94367cf91f7039a56889b457f2eac70
Bùi Văn Hiến <vanhienbui325@gmail.com>
2022-09-08 16:03:04 +0700

    change write

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Solution
+    {
+
+        static int solveMeFirst(int a, int b)
+        {
+            // Hint: Type return a+b; below  
+            return a + b;
+
+        }
+
+        static void Main(String[] args)
+        {
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int sum = solveMeFirst(val1, val2);
+            Console.WriteLine(sum);
+        }
+    }
+
+}
